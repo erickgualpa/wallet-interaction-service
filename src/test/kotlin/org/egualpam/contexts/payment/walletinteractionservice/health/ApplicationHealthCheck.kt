8a7ce74af833b92ca31c9a32cd43c263e9413d87
Erick Gualpa <m.erickgualpa@gmail.com>
2024-07-02 20:47:07 +0200

    Upate pom.xml file

--- src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/health/ApplicationHealthCheck.kt
+++ src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/health/ApplicationHealthCheck.kt
@@ -6,7 +6,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-class ApplicationHealthTest : AbstractIntegrationTest() {
+class ApplicationHealthCheck : AbstractIntegrationTest() {
 
   @Test
   fun `application status should be UP`() {
