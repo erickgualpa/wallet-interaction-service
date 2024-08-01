1624445d9bff3766cdc3eca060e445905df40b51
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-01 21:36:33 +0200

    Rename controller

--- src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/in/controllers/PutWalletController.kt
+++ src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/in/controllers/PutWalletController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/v1/wallets")
 @RestController
-class PostWalletController(
+class PutWalletController(
   private val createWallet: CreateWallet
 ) {
 
