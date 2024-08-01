60fc54437eab8d8d3742cf9e4d82b34eb949709f
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-02 00:30:47 +0200

    Add unit test for Wallet aggregate root identity

---
+++ src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/application/domain/WalletShould.kt
@@ -0,0 +1,31 @@
+package org.egualpam.contexts.payment.walletinteractionservice.wallet.application.domain
+
+import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.util.UUID.randomUUID
+
+class WalletShould {
+  @Test
+  fun `define its identity by aggregate id`() {
+    val walletId = randomUUID().toString()
+
+    val wallet = Wallet.create(
+        id = walletId,
+        ownerId = randomUUID().toString(),
+        ownerUsername = randomAlphabetic(5),
+        accountId = randomUUID().toString(),
+        accountCurrency = "EUR",
+    )
+
+    val walletInDifferentState = Wallet.create(
+        id = walletId,
+        ownerId = randomUUID().toString(),
+        ownerUsername = randomAlphabetic(5),
+        accountId = randomUUID().toString(),
+        accountCurrency = "EUR",
+    )
+
+    assertTrue(wallet == walletInDifferentState)
+  }
+}
