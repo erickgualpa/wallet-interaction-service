33578a7a04343c2a09906ef788e6d6695ed14d9e
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-02 21:35:32 +0200

    Update DomainEventId

--- src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/shared/application/domain/DomainEventId.kt
+++ src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/shared/application/domain/DomainEventId.kt
@@ -1,9 +1,18 @@
 package org.egualpam.contexts.payment.walletinteractionservice.shared.application.domain
 
+import org.egualpam.contexts.payment.walletinteractionservice.shared.application.domain.exceptions.InvalidDomainEntityId
 import java.util.UUID
 
-data class DomainEventId(val value: UUID) {
+data class DomainEventId(val value: String) {
+  init {
+    try {
+      UUID.fromString(value)
+    } catch (e: IllegalArgumentException) {
+      throw InvalidDomainEntityId(value)
+    }
+  }
+
   companion object {
-    fun generate() = DomainEventId(UUID.randomUUID())
+    fun generate() = DomainEventId(UUID.randomUUID().toString())
   }
 }
