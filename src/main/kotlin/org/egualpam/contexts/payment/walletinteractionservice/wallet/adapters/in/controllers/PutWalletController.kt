f577768a72de1d04fda4475867a18fcbcb003da7
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-02 21:59:02 +0200

    Replace transactional annotation by programmatic approach

--- src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/in/controllers/PutWalletController.kt
+++ src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/in/controllers/PutWalletController.kt
@@ -3,6 +3,7 @@ package org.egualpam.contexts.payment.walletinteractionservice.wallet.adapters.`
 import org.egualpam.contexts.payment.walletinteractionservice.shared.application.domain.exceptions.InvalidDomainEntityId
 import org.egualpam.contexts.payment.walletinteractionservice.wallet.application.usecases.command.CreateWallet
 import org.springframework.http.ResponseEntity
+import org.springframework.transaction.support.TransactionTemplate
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,14 +12,16 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/wallets")
 @RestController
 class PutWalletController(
+  private var transactionTemplate: TransactionTemplate,
   private val createWallet: CreateWallet
 ) {
 
   @PutMapping
   fun putWallet(@RequestBody putWalletRequest: PutWalletRequest): ResponseEntity<Void> {
     return try {
-      putWalletRequest.toCommand().let {
-        createWallet.execute(it)
+      val createWalletCommand = putWalletRequest.toCommand()
+      transactionTemplate.executeWithoutResult {
+        createWallet.execute(createWalletCommand)
       }
       ResponseEntity.noContent().build()
     } catch (e: InvalidDomainEntityId) {
--- src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/out/savewallet/SaveWalletSpringDataJdbcAdapter.kt
+++ src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/adapters/out/savewallet/SaveWalletSpringDataJdbcAdapter.kt
@@ -4,12 +4,10 @@ import org.egualpam.contexts.payment.walletinteractionservice.wallet.adapters.ou
 import org.egualpam.contexts.payment.walletinteractionservice.wallet.adapters.out.shared.springdatajdbc.WalletRepository
 import org.egualpam.contexts.payment.walletinteractionservice.wallet.application.domain.Wallet
 import org.egualpam.contexts.payment.walletinteractionservice.wallet.application.ports.out.SaveWallet
-import org.springframework.transaction.annotation.Transactional
 
 open class SaveWalletSpringDataJdbcAdapter(
   private var walletRepository: WalletRepository
 ) : SaveWallet {
-  @Transactional
   override fun save(wallet: Wallet) {
     WalletPersistenceEntity.from(wallet).let {
       walletRepository.save(it)
