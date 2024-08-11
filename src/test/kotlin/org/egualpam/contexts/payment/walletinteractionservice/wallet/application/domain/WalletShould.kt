88b5025b0c0b686ff3f41a7060162dfac847cae4
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-12 00:09:55 +0200

    Amend test

--- src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/application/domain/WalletShould.kt
+++ src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/wallet/application/domain/WalletShould.kt
@@ -1,10 +1,8 @@
 package org.egualpam.contexts.payment.walletinteractionservice.wallet.application.domain
 
 import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
-import org.egualpam.contexts.payment.walletinteractionservice.wallet.application.ports.out.WalletExists
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
 import java.util.UUID.randomUUID
 
 class WalletShould {
@@ -12,7 +10,6 @@ class WalletShould {
   fun `define its identity by aggregate id`() {
     val walletId = randomUUID().toString()
 
-    val walletExists = mock<WalletExists>()
     val wallet = Wallet.create(
         id = walletId,
         ownerId = randomUUID().toString(),
