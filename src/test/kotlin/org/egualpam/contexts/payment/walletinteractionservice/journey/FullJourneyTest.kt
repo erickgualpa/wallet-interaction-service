efce82fc38274180bb3e64ef9420af97af810020
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-15 13:40:25 +0200

    Add FullJourneyTest

---
+++ src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/journey/FullJourneyTest.kt
@@ -0,0 +1,64 @@
+package org.egualpam.contexts.payment.walletinteractionservice.journey
+
+import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
+import org.egualpam.contexts.payment.walletinteractionservice.shared.adapters.AbstractIntegrationTest
+import org.junit.jupiter.api.Test
+import org.testcontainers.shaded.com.google.common.net.HttpHeaders.CONTENT_TYPE
+import java.util.UUID.randomUUID
+import kotlin.random.Random.Default.nextDouble
+
+class FullJourneyTest : AbstractIntegrationTest() {
+  @Test
+  fun `full journey`() {
+    val walletId = randomUUID().toString()
+    val currency = "EUR"
+
+    val createWalletRequest = """
+      {
+        "wallet": {
+          "id": "$walletId",
+          "owner": {
+            "id": "${randomUUID()}",
+            "username": "${randomAlphabetic(10)}"
+          },
+          "account": {
+            "id": "${randomUUID()}",
+            "currency": "$currency"
+          }
+        }
+      }
+    """
+
+    webTestClient.put()
+        .uri("/v1/wallets")
+        .header(CONTENT_TYPE, "application/json")
+        .bodyValue(createWalletRequest)
+        .exchange()
+        .expectStatus()
+        .isNoContent
+
+    webTestClient.get()
+        .uri("/v1/wallets/{wallet-id}", walletId)
+        .exchange()
+        .expectStatus()
+        .isOk
+
+    val walletDepositRequest = """
+      {
+        "deposit": {
+          "id": "${randomUUID()}",
+          "amount": "${nextDouble(10.0, 10000.0)}",
+          "currency": "$currency"
+        }
+      }
+    """
+
+    webTestClient.put()
+        .uri("/v1/wallets/{wallet-id}/deposit", walletId)
+        .header(CONTENT_TYPE, "application/json")
+        .bodyValue(walletDepositRequest)
+        .exchange()
+        .expectStatus()
+        .isNoContent
+  }
+}
