052cb7a39dfb5901ee7aa337e1b8626244ae9caa
Erick Gualpa <m.erickgualpa@gmail.com>
2025-02-23 17:58:05 +0100

    Use rabbitMqEnvironment from WalletStreamTestConsumer

--- src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/shared/adapters/configuration/TestConsumersConfiguration.kt
+++ src/test/kotlin/org/egualpam/contexts/payment/walletinteractionservice/shared/adapters/configuration/TestConsumersConfiguration.kt
@@ -1,7 +1,6 @@
 package org.egualpam.contexts.payment.walletinteractionservice.shared.adapters.configuration
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.rabbitmq.stream.Address
 import com.rabbitmq.stream.Environment
 import org.egualpam.contexts.payment.walletinteractionservice.e2e.helper.WalletStreamTestConsumer
 import org.springframework.context.annotation.Bean
@@ -11,17 +10,7 @@ import org.springframework.context.annotation.Configuration
 class TestConsumersConfiguration {
   @Bean
   fun walletStreamTestConsumer(
-    rabbitMqProperties: RabbitMqProperties,
+    rabbitMqEnvironment: Environment,
     objectMapper: ObjectMapper
-  ): WalletStreamTestConsumer {
-    val environment = Environment.builder()
-        .addressResolver {
-          Address(rabbitMqProperties.host, rabbitMqProperties.streamPort)
-        }
-        .username(rabbitMqProperties.adminUsername)
-        .password(rabbitMqProperties.adminPassword)
-        .build()
-
-    return WalletStreamTestConsumer(environment, objectMapper)
-  }
+  ) = WalletStreamTestConsumer(rabbitMqEnvironment, objectMapper)
 }
