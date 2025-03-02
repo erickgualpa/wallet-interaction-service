ec94fbbdb0c13ec994686d186539d1b6b021dbd8
Erick Gualpa <m.erickgualpa@gmail.com>
2025-03-02 12:36:24 +0100

    Remove unneeded log

--- src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/account/application/ports/in/command/CreateAccount.kt
+++ src/main/kotlin/org/egualpam/contexts/payment/walletinteractionservice/account/application/ports/in/command/CreateAccount.kt
@@ -5,23 +5,16 @@ import org.egualpam.contexts.payment.walletinteractionservice.account.applicatio
 import org.egualpam.contexts.payment.walletinteractionservice.account.application.ports.out.AccountExists
 import org.egualpam.contexts.payment.walletinteractionservice.account.application.ports.out.AccountRepository
 import org.egualpam.contexts.payment.walletinteractionservice.shared.application.ports.out.EventBus
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 class CreateAccount(
   private val accountExists: AccountExists,
   private val repository: AccountRepository,
   private val eventBus: EventBus
 ) {
-
-  private val logger: Logger = LoggerFactory.getLogger(this::class.java)
-
   fun execute(command: CreateAccountCommand) {
     val accountId = AccountId(command.accountId)
 
     if (accountExists.with(accountId)) {
-      // TODO: Remove this log once migration completed
-      logger.info("Account with id: [${accountId.value}] already exists")
       return
     }
 
